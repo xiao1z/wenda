5482d6a9431a08dcc850b4128718d46cde6aa79f
zhangxiao <1042083931@qq.com>
2017-06-22 17:47:59 +0800

    zhangxiao

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,8 +1,15 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
-
+	@RequestMapping("/")
+	@ResponseBody
+	public String hello()
+	{
+		return "hello";
+	}
 }
